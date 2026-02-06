0776d06195629df6733a94a1dec9994eae7beab1
Julian <j.florez078@hotmail.com>
2026-02-05 23:15:19 -0500

    Implementación de modelos y repositorios

---
+++ src/main/java/com/Dev_Learning_Lab/library_system/repository/LibraryRepository.java
@@ -0,0 +1,14 @@
+package com.Dev_Learning_Lab.library_system.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.Dev_Learning_Lab.library_system.model.LibraryModel;
+import java.util.List;
+
+public interface LibraryRepository extends MongoRepository<LibraryModel, String> {
+
+    List<LibraryModel> findByNameIgnoreCase(String name);
+
+    List<LibraryModel> findByDescription(String description);
+
+    List<LibraryModel> findByDate(String date);
+}
