8ee04eb533d5961ec0ad5680e6e51eb6b9807f85
Julian <j.florez078@hotmail.com>
2026-02-07 14:09:58 -0500

    Implementation of LibraryService

--- src/main/java/com/Dev_Learning_Lab/library_system/service/LibraryService.java
+++ src/main/java/com/Dev_Learning_Lab/library_system/service/LibraryService.java
@@ -1,6 +1,29 @@
 package com.Dev_Learning_Lab.library_system.service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.Dev_Learning_Lab.library_system.model.LibraryModel;
+import com.Dev_Learning_Lab.library_system.repository.LibraryRepository;
+
+@Service
 public class LibraryService {
-    
+
+    private final LibraryRepository libraryRepository;
+
+    public LibraryService(LibraryRepository libraryRepository) {
+        this.libraryRepository = libraryRepository;
+    }
+
+    public List<LibraryModel> getByName(String name) {
+        return libraryRepository.findByNameIgnoreCase(name);
+    }
+
+    public List<LibraryModel> getByDate(String date) {
+        return libraryRepository.findByDate(date);
+    }
+
+    public List<LibraryModel> getByDescription(String description) {
+        return libraryRepository.findByDescription(description);
+    }
 }
-    
