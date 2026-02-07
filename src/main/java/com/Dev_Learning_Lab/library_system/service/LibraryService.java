e3b772871cc18c27dacd4f3fe4839282f7a64f18
Julian <j.florez078@hotmail.com>
2026-02-07 17:41:24 -0500

    Update frontend files and service

--- src/main/java/com/Dev_Learning_Lab/library_system/service/LibraryService.java
+++ src/main/java/com/Dev_Learning_Lab/library_system/service/LibraryService.java
@@ -1,6 +1,28 @@
 package com.Dev_Learning_Lab.library_system.service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.Dev_Learning_Lab.library_system.model.LibraryModel;
+import com.Dev_Learning_Lab.library_system.repository.LibraryRepository;
+
+@Service
 public class LibraryService {
-    
+
+    private final LibraryRepository libraryRepo;
+
+    public LibraryService(LibraryRepository libraryRepo) {
+        this.libraryRepo = libraryRepo;
+    }
+
+    public List<LibraryModel> getByName(String name) {
+        return libraryRepo.findByNameIgnoreCase(name);
+    }
+
+    public List<LibraryModel> getByDate(String date) {
+        return libraryRepo.findByDate(date);
+    }
+
+    public List<LibraryModel> getByDescription(String description) {
+        return libraryRepo.findByDescription(description);
+    }
 }
-    
