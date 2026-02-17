ab7119932b1b79a5434be0a4dc1acf73118ce882
Julian <j.florez078@hotmail.com>
2026-02-17 03:07:25 -0500

    feat: add date range search to LibraryService

--- src/main/java/com/Dev_Learning_Lab/library_system/service/LibraryService.java
+++ src/main/java/com/Dev_Learning_Lab/library_system/service/LibraryService.java
@@ -14,6 +14,23 @@ public class LibraryService {
         this.libraryRepo = libraryRepo;
     }
 
+    public LibraryModel getBookById(String id) {
+        return libraryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    public List<LibraryModel> getBookByAutor(String autor) {
+        return libraryRepo.findByAutor(autor);
+    }
+
+    public LibraryModel createBook(LibraryModel book) {
+        return libraryRepo.save(book);
+    }
+
+    public List<LibraryModel> getAllbooks() {
+        return libraryRepo.findAll();
+    }
+
     public List<LibraryModel> getByName(String name) {
         return libraryRepo.findByNameIgnoreCase(name);
     }
@@ -24,5 +41,66 @@ public class LibraryService {
 
     public List<LibraryModel> getByDescription(String description) {
         return libraryRepo.findByDescription(description);
+    }
+
+    public List<LibraryModel> getByCategory(String category) {
+        return libraryRepo.findByCategoryIgnoreCase(category);
+    }
+
+    public LibraryModel updateBook(String id, LibraryModel updatedBook) {
+        LibraryModel book = libraryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        book.setName(updatedBook.getName());
+        book.setDescription(updatedBook.getDescription());
+        book.setCategory(updatedBook.getCategory());
+        book.setAutor(updatedBook.getAutor());
+        book.setCopiesAvailable(updatedBook.getCopiesAvailable());
+        book.setTotalCopies(updatedBook.getTotalCopies());
+        book.setDate(updatedBook.getDate());
+
+        return libraryRepo.save(book);
+    }
+
+    public void deleteBook(String id) {
+        LibraryModel book = libraryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+        libraryRepo.delete(book);
+    }
+
+    public LibraryModel rentBook(String id) {
+        LibraryModel book = libraryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        if (book.getCopiesAvailable() > 0) {
+            book.setCopiesAvailable(book.getCopiesAvailable() - 1);
+        } else {
+            throw new RuntimeException("The book is not available");
+        }
+
+        libraryRepo.save(book);
+        return book;
+    }
+
+    public LibraryModel returnBook(String id) {
+        LibraryModel book = libraryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        if (book.getCopiesAvailable() < book.getTotalCopies()) {
+            book.setCopiesAvailable(book.getCopiesAvailable() + 1);
+        } else {
+            throw new RuntimeException("The book has all copies in the library");
+        }
+
+        libraryRepo.save(book);
+        return book;
+    }
+
+    public List<LibraryModel> getAvailableBooks() {
+        return libraryRepo.findByCopiesAvailableGreaterThan(0);
+    }
+
+    public List<LibraryModel> getBooksByDateRange(String fromDate, String toDate) {
+        return libraryRepo.findByDateBetween(fromDate, toDate);
     }
 }
